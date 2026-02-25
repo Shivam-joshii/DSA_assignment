bcee269fb0d280eb3813855d335bee7d99fe48f5
Shivam-joshii <shivam.jooshii@gmail.com>
2026-02-25 09:12:18 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, key, i, count = 0;
+    struct Node *head = NULL, *temp = NULL, *newnode;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        newnode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+
+        if(head == NULL) {
+            head = newnode;
+            temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    temp = head;
+    while(temp != NULL) {
+        if(temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
